5a47f319d95be3f901cc6ce5861dcbf1550fbda1
Marcus Soares <marcus@asolar.es>
2016-05-10 16:09:27 -0300

    Enviando e-mail com 2 anexos.

--- application/controllers/utils.php
+++ application/controllers/utils.php
@@ -19,19 +19,37 @@ class Utils extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->model('utils_model', 'utils');
-		$this->data['database'] = $this->utils->get_database();
-		$this->data['tables'] = $this->utils->check_tables();
-		
-		$this->load->view('index', $this->data);
+		$this->load->library('email');
+
+		$this->email->from('marcussoares16@gmail.com', 'Marcus Soares');
+		$this->email->to('marcusoares16@gmail.com');
+
+		$this->email->subject('Teste vigilancia');
+		$this->email->message('Testando a classe de email.');	
+	    $this->email->attach($this->varredura(0));
+	    $this->email->attach($this->varredura(1));
+
+		echo $this->email->send() ? 'E-mail enviado!' : 'Problemas ao enviar e-mail.';
+
+		//echo $this->email->print_debugger();
+		//$this->load->view('index', $this->data);
 	}
 
-	public function consulta()
+	public function varredura($indice = 0)
 	{
-		$this->load->model('utils_model', 'utils');
-		$consulta = $this->utils->select_estoque();
-		
-		$this->load->view('consulta', compact('consulta'));
+		$path = 'D:/teste/';
+		$diretorio = glob($path.'*.{jpeg,gif,png}');   
+
+		$arquivos = array();
+		foreach (glob($path.'*.{jpeg,jpg}', GLOB_BRACE) as $file) {
+			//$file." \n\r";
+			$arquivos[filectime($file)] = $file;
+			krsort($arquivos);
+			//print_r(array_slice($arquivos,0,2));
+			$anexo = array_slice($arquivos,0,2);
+		}
+
+		return isset($anexo[$indice]) ? "".$anexo[$indice] : null;
 	}
 }
 
---
+++ prompt_envio_msg.bat
@@ -0,0 +1,5 @@
+d:
+cd \xampp\htdocs\vig
+php index.php tools message
+
+pause
